8f15e778229ad686783f3bf308875eb325184c8e
liangwei <lw11250802@gmail.com>
2026-07-13 18:34:05 +0800

    feat(plugin): add artifact output helpers

---
+++ packages/plugin-artifacts/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  artifactAttachment,
+  requireReadableInput,
+  resolveArtifactOutput,
+  type ArtifactOutput,
+  type OutputInput,
+} from "./output.js"
+export { normalizeExtractedText, requireExpectedText } from "./text.js"
